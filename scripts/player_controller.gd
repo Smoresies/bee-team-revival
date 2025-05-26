f6afa19477007831f0cdfd19859a5b544641d07c
Smoresies <gavin.britton@gmail.com>
2025-05-26 16:12:18 -0700

    Initial setup of project, movement, and play controller

---
+++ scripts/player_controller.gd
@@ -0,0 +1,83 @@
+extends CharacterBody2D
+
+@export var MAX_SPEED: int = 200
+@export var JUMP_VELOCITY: int = -300.0
+@export var ACCELERATION: int = 1500
+@export var DASH_POWER: int = 2000
+@export_range(0, 10, 0.1, "or_greater") var DECELERATION_TIME: float = 0.1
+
+var deceleration: float = MAX_SPEED / DECELERATION_TIME
+
+var horizontal_facing: int = 1
+
+var double_jump: bool = true
+var grounded: bool = true
+var can_dash: bool = true
+var dashing: bool = false
+
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
+	
+	apply_player_jump()
+	
+	apply_horizontal_movement(delta)
+	
+	apply_player_dash()
+
+	move_and_slide()
+
+
+func apply_gravity(delta: float):
+	# Add the apply_gravity.
+	if not is_on_floor() and not dashing:
+		grounded = false
+		velocity += get_gravity() * delta
+	# this elif checks if we are on floor and weren't last tick
+	elif grounded == false:
+		_update_grounded_flags()
+
+# Checks for jump input and jumps if either grounded or have a double jump
+# Using local boolean rather than is_on_floor function call. 
+# Likely unneeded Micro-Optimization, but is used for other checks as well
+func apply_player_jump():
+	if Input.is_action_just_pressed("jump"):
+		if grounded:
+			velocity.y = JUMP_VELOCITY
+		elif double_jump:
+			velocity.y = JUMP_VELOCITY
+			double_jump = false
+
+func apply_horizontal_movement(delta: float):
+	
+	
+	if not dashing:
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("left", "right")
+
+		if direction > 0:
+			horizontal_facing = 1
+		elif direction < 0:
+			horizontal_facing = -1
+		# In case of overspeed from dash
+		if abs(velocity.x) > MAX_SPEED:
+			velocity.x = (horizontal_facing * MAX_SPEED) / 2
+		if direction: # Direction button active
+			# Accelerate towards the target speed
+			velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
+		else: # Direction button inactive
+			# Decelerate towards zero speed
+			velocity.x = lerp(velocity.x, 0.0, DECELERATION_TIME)
+
+func apply_player_dash():
+	if Input.is_action_just_pressed("dash") and can_dash:
+		can_dash = false
+		dashing = true
+		velocity.x = horizontal_facing * DASH_POWER
+		await get_tree().create_timer(0.1).timeout
+		can_dash = true
+		dashing = false 
+
+# Called when we have just become grounded. Updates all flags for being grounded.
+func _update_grounded_flags():
+	grounded = true
+	double_jump = true
